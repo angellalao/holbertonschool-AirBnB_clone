805ea8a9d816aeb3bf20e1e7afca32686094a87a
BryanField15 <6367@holbertonstudents.com>
2023-07-03 23:25:13 -0700

    first test failed

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,12 @@
 import unittest
-from models.base import Base
+from models.base_model import BaseModel
+
 
 class TestBase(unittest.TestCase):
-    """Tests for the  Base class""
+    """Tests for the  Base class"""
+
+    def test_str(self):
+        """Test that it  returns a formatted string"""
+        b1 = BaseModel()
+        self.assertEqual(str(b1),
+- [<class 'models.base_model.BaseModel'>] (b35598e0-62dd-4482-aabb-a2b1ea22eaad) {'id': 'b35598e0-62dd-4482-aabb-a2b1ea22eaad', 'created_at': '2023-07-03T23:21:56.213328', 'updated_at': '2023-07-03T23:21:56.213335', '__class__': 'BaseModel'})
