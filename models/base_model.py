71443b753c07db290fa791942660d26bc9b0db39
BryanField15 <6367@holbertonstudents.com>
2023-07-03 20:34:08 -0700

    first crack

---
+++ models/base_model.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module with class Base"""
+
+import json
+import uuid
+from datetime import datetime
+
+
+class Base:
+    """Defines all common attributes/methods for other classes"""
+
+    def __init__(self):
+        """Initialize class attibutes"""
+        self.id = uuid.uuid4()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Prints formatted string"""
+        return (f"[{type(self)}] ({self.id}) {self.to_dict()}")
+
+    def save(self):
+        """Updates public instance attribute updated_at with current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns the dictionary representation of an instance """
+        return {
+            "id": self.id,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat(),
+            }
