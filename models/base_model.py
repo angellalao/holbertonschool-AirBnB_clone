d3222a2c4c4faffad605edebf0282c98cec02fb9
BryanField15 <6367@holbertonstudents.com>
2023-07-03 22:15:08 -0700

    completed task 3

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ class BaseModel:
 
     def __init__(self):
         """Initialize class attibutes"""
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
@@ -26,5 +26,7 @@ class BaseModel:
     def to_dict(self):
         """Returns the dictionary representation of an instance """
         new_dict = self.__dict__.copy()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
         new_dict["__class__"] = type(self).__name__
         return new_dict
