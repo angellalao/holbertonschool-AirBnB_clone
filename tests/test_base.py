836d578d1c01d807407abf49269819c8445492c6
BryanField15 <6367@holbertonstudents.com>
2023-07-05 03:31:57 -0700

    new tests

--- tests/test_base.py
+++ tests/test_base.py
@@ -2,11 +2,30 @@ import unittest
 from models.base_model import BaseModel
 
 
-class TestBase(unittest.TestCase):
-    """Tests for the  Base class"""
+class TestBaseModel(unittest.TestCase):
+    """Tests for the BaseModel class"""
 
-    def test_str(self):
-        """Test that it  returns a formatted string"""
-        b1 = BaseModel()
-        expected_formatted_string = f"[{b1.__class__}] ({b1.id}) {{'id': '{b1.id}', 'created_at': '{b1.created_at.isoformat()}', 'updated_at': '{b1.updated_at.isoformat()}', '__class__': '{type(b1).__name__}'}}"
-        self.assertEqual(str(b1), expected_formatted_string)
+    def setUp(self):
+        """Test that class can be instatiated"""
+        self.b1 = BaseModel()
+        self.assertIsInstance(self.b1, BaseModel)
+
+    def test_str_returns_a_string(self):
+        """Test that __str__ returns type str"""
+        model_string = self.b1.__str__()
+        self.assertIsInstance(model_string, str)
+
+    def test_str_returns_the_expected_format(self):
+        """Test that __str__ returns a formatted string"""
+        expected_formatted_string = (
+            f"[{self.b1.__class__}] ({self.b1.id}) {{'id': '{self.b1.id}', "
+            f"'created_at': '{self.b1.created_at.isoformat()}', 'updated_at': "
+            f"'{self.b1.updated_at.isoformat()}', '__class__': "
+            f"'{type(self.b1).__name__}'}}"
+            )
+        self.assertEqual(str(self.b1), expected_formatted_string)
+
+    def test_save_correctly_updates_updated_at_time(self):
+        """Test that save correctly updates the updated_at time"""
+        expected_updated_time = self.b1.updated_at.isoformat()
+        self.assertEqual(expected_updated_time, self.b1.updated_at.isoformat())
