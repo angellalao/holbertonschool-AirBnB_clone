57cc9ff62394d03c627d3e191a64b8adbce5c15c
angellalao <6369@holbertonstudents.com>
2023-07-05 04:42:41 -0700

    add args and kwargs parameters

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ from datetime import datetime
 class BaseModel:
     """Defines all common attributes/methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize class attibutes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
