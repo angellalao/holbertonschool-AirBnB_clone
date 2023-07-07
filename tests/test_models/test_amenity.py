10b49b7046473bce09bc8d5738abf26f1fe251bc
BryanField15 <6367@holbertonstudents.com>
2023-07-07 02:49:10 -0700

    fixed indent

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,11 +2,11 @@ import unittest
 from models.amenity import Amenity
 
 class TestAmenity(unittest.TestCase):
-    """Tests for the Amenity  class"""
+    """Tests for the Amenity class"""
 
     def setUp(self):
         """Test that class can be instatiated"""
-                self.a1 = Amenity()
+        self.a1 = Amenity()
 
     def test_is_instance(self):
         """Test that an instance is of type Amenity"""
