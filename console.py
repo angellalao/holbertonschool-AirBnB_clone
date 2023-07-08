06aef11e39bf70d92a0c749a0563502a85f5487c
angellalao <6369@holbertonstudents.com>
2023-07-07 20:14:19 -0700

    create method finished

--- console.py
+++ console.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Entry point of the command interpreter """
 import cmd
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -18,6 +20,17 @@ class HBNBCommand(cmd.Cmd):
         """Quit command to exit the program\n"""
         return True
 
+    def do_create(self, args):
+        """Creates a new instance of BaseModel, saves it to a json\
+file and prints the id\n"""
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args != "BaseModel":\
+            print("** class doesn't exist **")
+        else:
+            new_instance = BaseModel()
+            new_instance.save()
+            print(new_instance.id)
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
