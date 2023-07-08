514ad2d10675dc14bb40fc23bac7f40e50d7c7df
angellalao <6369@holbertonstudents.com>
2023-07-07 19:26:29 -0700

    remove whitespace/ add blankline

--- console.py
+++ console.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-import cmd, sys
+import cmd
+"""Entry point of the command interpreter """
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def emptyline(self):
-        """ Override emptyline to do nothing """
+        """ Override emptyline to do nothing\n"""
         pass
 
     def do_EOF(self, args):
-        """Exits the program"""
+        """Exits the program\n"""
         raise SystemExit
 
     def do_quit(self, args):
-        """Quit command to exit the program"""
+        """Quit command to exit the program\n"""
         return True
-
 
 
 if __name__ == '__main__':
